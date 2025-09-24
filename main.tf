0b750e38303231da6acb6ed1f3030afa0a5d9d14
sohanyadav <sohanyadavdevops@gmail.com>
2025-09-24 12:10:28 +0530

    update tag

--- main.tf
+++ main.tf
@@ -8,53 +8,47 @@ resource "random_password" "password" {
 module "postgresql" {
   source = "git::https://github.com/opszero/terraform-aws-rds.git?ref=v1.0.1"
 
-  #lables
   name = var.name
 
-  #networking
+  # networking
   allowed_ip          = var.allowed_ip
   allowed_ports       = var.allowed_ports
   vpc_id              = var.vpc_id
   subnet_ids          = var.subnet_ids
   publicly_accessible = var.publicly_accessible
 
-  #storage
+  # storage
   allocated_storage = var.allocated_storage
   storage_encrypted = true
 
-  #backups
+  # backups
   backup_retention_period = 7
   backup_window           = "03:00-06:00"
   maintenance_window      = "Mon:00:00-Mon:03:00"
 
-  #logs
+  # logs
   enabled_cloudwatch_logs_exports = var.enabled_cloudwatch_logs_exports
 
-
-
-  #database
+  # database
   db_name        = var.db_name
   db_username    = var.username
-  password       = var.password == "" ? join("", random_password.password.*.result) : var.password
+  password       = var.password == "" ? random_password.password[0].result : var.password
   port           = "5432"
   instance_class = var.instance_class
 
-  #engine
+  # engine
   engine               = "postgres"
   engine_name          = "postgres"
   engine_version       = "15.5"
   family               = "postgres15"
   major_engine_version = "15"
 
-  #other
+  # other
   multi_az                       = false
   ssm_parameter_endpoint_enabled = false
   deletion_protection            = false
   apply_immediately              = true
-
-
 }
-
 
 resource "helm_release" "metabase" {
   depends_on       = [module.postgresql]
@@ -65,6 +59,7 @@ resource "helm_release" "metabase" {
   repository       = "https://pmint93.github.io/helm-charts"
   version          = var.metabase_version
   values           = [file("metabase.yaml")]
+
   set = [
     {
       name  = "database.host"
@@ -72,6 +67,7 @@ resource "helm_release" "metabase" {
     },
     {
       name  = "database.password"
-      value = var.password == "" ? join("", random_password.password.*.result) : var.password
-  }]
+      value = var.password == "" ? random_password.password[0].result : var.password
+    }
+  ]
 }
